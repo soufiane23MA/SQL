7c393b8aa5805a82a38ec64da3f7a86c2ba08c3f
soufiane23Ma <soufiane.elmaarouf@gmail.com>
2024-11-26 13:32:36 +0100

    EXO_SQL_CINEMA

---
+++ EXO_SQL_CINEMA.sql
@@ -0,0 +1,49 @@
+EXO_SQL_CINEMA
+
+a: Informations d’un film (id_film) : titre, année, durée (au format HH:MM) et
+réalisateur ;
+
+SELECT titre, annee_Sortie, duree_Filme, id_Realisateur FROM film f
+INNER JOIN personne p ON f.id_Realisateur = p.id_Personne;
+
+b: Liste des films dont la durée excède 2h15 classés par durée (du + long au + court)
+ 
+SELECT f.titre AS 'TITRE', f.duree_Filme AS 'DUREE DE Filme' FROM film f 
+WHERE  duree_Filme > '02:15:00'
+ORDER BY  duree_Filme DESC;
+
+c: Liste des films d’un réalisateur (en précisant l’année de sortie) 
+/* ici on cherche a affiché l'enssemble de films d'un réalisateur précis, et l'année de sortie de filme */
+
+
+SELECT f.titre AS Titre, f.annee_Sortie AS "Année de sortie",nom_Personne AS "nom Realisateur"
+FROM film f
+JOIN realisateur r ON f.id_Realisateur = r.id_Realisateur
+JOIN personne p ON r.id_Personne = p.id_Personne
+WHERE r.id_Realisateur = 1;
+
+d. Nombre de films par genre (classés dans l’ordre décroissant)
+/* pour compter le nombre de filme par genre, il faut utiliser la fonction d'agrégation count(), 
+/* pour l'ordre décroissant on utilisra DESC
+*/
+
+SELECT g.libelle AS genre,COUNT(f.id_Filme) AS nombre_films
+FROM genre g
+INNER JOIN appartenir ap ON g.id_Genre = ap.id_Genre
+INNER JOIN film f ON ap.id_Filme = f.id_Filme
+GROUP BY g.libelle
+ORDER BY nombre_films DESC;
+
+e. Nombre de films par réalisateur (classés dans l’ordre décroissant)
+/*  dans la table filme , on compte le nombre de filme regrouper par le nom du realisateur, 
+avec DESC on obtien l'ordre décroissant .*/
+
+
+SELECT p.nom_Personne AS 'Realisateur',COUNT(f.id_Filme) AS 'nombre_films'
+FROM film f
+INNER JOIN realisateur r ON r.id_Realisateur = f.id_Realisateur
+INNER JOIN personne p ON r.id_Personne = p.id_Personne
+GROUP BY p.nom_Personne
+ORDER BY nombre_films DESC;
+
+f. Casting d’un film en particulier (id_film) : nom, prénom des acteurs + sexe
